2f0a2442f410f0abbd6e72944d0e44dafe89ba3b
Tushar Sharma <108209199+tusharr03@users.noreply.github.com>
2024-04-02 04:42:56 +0530

    Create Inline_Function.cpp

---
+++ CodeStudio_GuidedPath/Inline_Function.cpp
@@ -0,0 +1,25 @@
+/*Disadvantages of using a normal function:
+
+It uses time as it pauses some portion of code to complete
+unless we are done with it; we can’t move forward.
+Creates a copy of variables each time while calling a
+function.
+
+
+To prevent this, what we can do is create inline 
+functions. Inline functions replace the function 
+call with its definition of when invoked.*/
+
+//declaration of inline function
+#include<iostream>
+using namespace std;
+
+inline func()
+{
+       
+}
+
+int main()
+{
+    return 0;
+}
